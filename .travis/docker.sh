3d35c7124756751ddcba3a5be9c4e26cf773e4cd
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-11-03 06:44:10 -0800

    Update docker.sh

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -35,6 +35,9 @@ mv yuzu-windows-msvc-source-* yuzu/
 
 cd /tmp/source/yuzu/
 
+curl -sLO https://raw.githubusercontent.com/curl/curl/master/CMake/FindMbedTLS.cmake ./externals/find-modules/
+ls -al ./externals/find-modules/
+
 find -path ./dist -prune -o -type f -exec sed -i 's/\r$//' {} ';'
 wget https://raw.githubusercontent.com/PineappleEA/Pineapple-Linux/master/inject-git-info.patch
 patch -p1 < inject-git-info.patch
@@ -44,7 +47,7 @@ mkdir -p build && cd build
 #curl -sL "https://raw.githubusercontent.com/yuzu-emu/yuzu/master/src/input_common/sdl/sdl_impl.cpp" -o /tmp/source/yuzu/src/input_common/sdl/sdl_impl.cpp
 
 cmake .. -G Ninja -DYUZU_USE_BUNDLED_UNICORN=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DTITLE_BAR_FORMAT_IDLE="yuzu Early Access $title" -DTITLE_BAR_FORMAT_RUNNING="yuzu Early Access $title | {3}" -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON -DGIT_BRANCH="HEAD" -DGIT_DESC="$msvc" -DUSE_DISCORD_PRESENCE=ON
-cp MbedTLSConfig.cmake mbedtlsConfig.cmake
+
 ninja -j $(nproc)
 
 #cat yuzu/build/CMakeFiles/CMakeError.log | curl -F 'f:1=<-' ix.io
