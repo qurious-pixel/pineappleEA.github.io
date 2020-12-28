5cfa90befc0de10cd0366d243fe699906985f4b6
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-12-28 11:44:55 -0800

    Update docker.sh

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -5,7 +5,7 @@ BRANCH=`echo ${GITHUB_REF##*/}`
 curl -s https://raw.githubusercontent.com/pineappleEA/pineappleEA.github.io/master/index.html > sourcefile.txt
 latest=$(cat sourcefile.txt | grep https://anonfiles.com/ | cut -d '=' -f 2 | cut -d '>' -f 1 | head -n 1)
 export title="Yuzu $(echo $latest | cut -d '-' -f 2 | cut -d '_' -f 1)"
-echo $title > $TRAVIS_BUILD_DIR/title.txt
+echo $title > $GITHUB_WORKSPACE/title.txt
 
 QT_BASE_DIR=/opt/qt514
 export QTDIR=$QT_BASE_DIR
